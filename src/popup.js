de2750a4db2f2d646b861bf364f584331ef2ea42
MatthewR <matthew.richardson@biton.com>
2025-08-19 11:07:39 +0100

    v0.3.0: chrome.storage api now stores recent items for each mode and allows revisiting on click

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,29 @@
 'use strict';
+import './popup.css'
 
 (function() {
+  
+  const addToHistory = async (type, value) => {
+    const linkHistory = await chrome.storage.local.get("history");
+    let history = linkHistory?.history || {number: [], text: []};
+    const itemsArr = history[type];
+    itemsArr.unshift(value);
+    itemsArr.splice(10);
+    // history[type] = itemsArr
+    await chrome.storage.local.set({history});
+  } 
   
   function navigateByItem(){
     const itemNumber = document.getElementById("redmine-number").value
     const href = `http://10.1.101.98:8080/redmine/issues/${itemNumber}`
+    addToHistory("number", itemNumber);
     window.open(href)
   }
 
-  function navigateByText(){
+  async function navigateByText(){
     const itemText = document.getElementById("redmine-text").value
     const href = `http://10.1.101.98:8080/redmine/search?issues=1&q="${itemText}"`
+    addToHistory("text", itemText);
     window.open(href)
   }
 
@@ -44,7 +57,7 @@
         document.getElementById("item-number-view").style.display = "flex";
         document.getElementById('redmine-number').focus();
         options.mode = "number";
-        await chrome.storage.local.set({options: options});
+        await chrome.storage.local.set({options});
         break;
       case "text":
         document.getElementById("text-search-view").style.display = "flex";
@@ -67,11 +80,38 @@
     })
   }
 
+  const retrieveSavedItems = async () => {
+    const savedItems = await chrome.storage.local.get("history");
+    // add recent number searches to a list
+    const numberDiv = document.getElementById("recent-number-items")
+
+    for (const item of (savedItems?.history?.number || [])){
+      const link = `http://10.1.101.98:8080/redmine/issues/${item}`;
+      const newLink = document.createElement('p')
+      newLink.className = "item-link"
+      newLink.onclick = () => window.open(link)
+      newLink.innerText = item;
+      numberDiv.appendChild(newLink);
+    }
+    // add recent text searches to a list
+    const textDiv = document.getElementById("recent-text-items")
+
+    for (const item of (savedItems?.history?.text || [])){
+      const link = `http://10.1.101.98:8080/redmine/search?issues=1&q="${item}"`;
+      const newLink = document.createElement('p')
+      newLink.className = "item-link"
+      newLink.onclick = () => window.open(link)
+      newLink.innerText = item;
+      textDiv.appendChild(newLink);
+    }
+  }
+
   document.addEventListener('DOMContentLoaded', async function(){
     setUpButtons();
     setUpModeBtns();
-    const savedData = await chrome.storage.local.get("options")
+    const savedData = await chrome.storage.local.get("options");
     if (savedData?.options?.mode === "text") switchView("text");
     else switchView("number");
+    await retrieveSavedItems();
   });
 })();
